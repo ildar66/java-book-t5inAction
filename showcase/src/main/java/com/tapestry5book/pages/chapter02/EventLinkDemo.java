258d57cc0a00d80ee617dc572ef48821d25f028e
ildar shafigullin <ildar66@inbox.ru>
2016-10-23 13:30:29 +0300

    2.4.5 Triggering events using the EventLink component

---
+++ showcase/src/main/java/com/tapestry5book/pages/chapter02/EventLinkDemo.java
@@ -0,0 +1,19 @@
+package com.tapestry5book.pages.chapter02;
+
+import org.apache.tapestry5.annotations.Persist;
+import org.apache.tapestry5.annotations.Property;
+
+public class EventLinkDemo {
+
+	@Property
+	@Persist
+	private int number;
+
+	void onIncrement() {
+		this.number++;
+	}
+
+/*	void onEventlink() {
+		this.number++;
+	}*/
+}
