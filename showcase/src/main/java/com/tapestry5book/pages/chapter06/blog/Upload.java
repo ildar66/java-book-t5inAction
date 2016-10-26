7f1c1f030e0a1e279b74eac5ce135a1b4889af44
ildar shafigullin <ildar66@inbox.ru>
2016-10-26 19:40:04 +0300

    Handling upload failures

--- showcase/src/main/java/com/tapestry5book/pages/chapter06/blog/Upload.java
+++ showcase/src/main/java/com/tapestry5book/pages/chapter06/blog/Upload.java
@@ -1,23 +1,32 @@
 package com.tapestry5book.pages.chapter06.blog;
 
+import java.io.File;
+
+import org.apache.commons.fileupload.FileUploadException;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.upload.services.UploadedFile;
 
-import java.io.File;
-
 public class Upload {
 
-    @Property
-    private UploadedFile file;
+	@Property
+	private UploadedFile file;
 
-    public void onSuccess() {
+	public void onSuccess() {
 
-        String tempDir = System.getProperty("java.io.tmpdir");
-        //System.out.println("==============" + tempDir);
-        File targetFile = new File(tempDir, file.getFileName());
+		String tempDir = System.getProperty("java.io.tmpdir");
+		// System.out.println("==============" + tempDir);
+		File targetFile = new File(tempDir, file.getFileName());
 
-        file.write(targetFile);
-    }
+		file.write(targetFile);
+	}
 
+	// Handling upload failures
+	@Property
+	private String uploadErrorMessage;
+
+	Object onUploadException(FileUploadException ex) {
+		uploadErrorMessage = "Upload exception: " + ex.getMessage();
+		return this;
+	}
 
 }
