53e0584c1ed9b12598d4f08c3b72279d8daf42a2
ildar shafigullin <ildar66@inbox.ru>
2016-10-28 23:52:03 +0300

    9.3.1 Understanding rendering phases

--- showcase/src/main/java/com/tapestry5book/components/Duke.java
+++ showcase/src/main/java/com/tapestry5book/components/Duke.java
@@ -8,16 +8,25 @@ import org.apache.tapestry5.ioc.annotations.Inject;
 
 public class Duke {
 
-    @Inject
-    @Path("duke.jpg")
-    private Asset duke;
+	@Inject
+	@Path("duke.jpg")
+	private Asset duke;
 
-    @BeginRender
-    boolean renderImage(MarkupWriter writer) {
-        writer.element("img", "src", duke);
+	/*
+	 * Returning false aborts the normal flow, causing a transition to a complementary phase. For example,
+	 * returning false in BeginRender results in a transition to CleanupRender; all the phases in between are skipped.
+	 * 
+	 * Instead of returning a boolean value, a render phase method may be void.
+	 * A void method is equivalent to a method returning true, and so doesn't skip the succeeding phase.
+	 * 
+	 * Yet another equivalent for returning true is to change method's return type to Object and return null
+	 */
+	@BeginRender
+	boolean renderImage(MarkupWriter writer) {
+		writer.element("img", "src", duke);
 
-        writer.end();
+		writer.end();
 
-        return false;
-    }
+		return false;
+	}
 }
