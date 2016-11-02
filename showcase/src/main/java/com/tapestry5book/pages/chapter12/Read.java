3d4d19bcddfd631d7ce1953101b76134b3faa627
ildar shafigullin <ildar66@inbox.ru>
2016-11-02 14:28:47 +0300

    Configuring Hibernate. Obtaining Hibernate session in pages and components

--- showcase/src/main/java/com/tapestry5book/pages/chapter12/Read.java
+++ showcase/src/main/java/com/tapestry5book/pages/chapter12/Read.java
@@ -4,10 +4,16 @@ import com.tapestry5book.entities.Article;
 import org.apache.tapestry5.annotations.PageActivationContext;
 import org.apache.tapestry5.annotations.Property;
 
+/**
+ * Built-in ValueEncoders for Hibernate entities Using the Hibernate integration library you don't need to
+ * contribute ValueEncoders for your Hibernate entities. This is done for you automatically. For every
+ * Hibernate entity class, Tapestry contributes a ValueEncoder that converts an entity instance into a
+ * client-side value by coercing entity's primary key into a String.
+ */
 public class Read {
 
-    @PageActivationContext
-    @Property
-    private Article article;
+	@PageActivationContext
+	@Property
+	private Article article;
 
 }
