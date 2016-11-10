864585ff3c049c339e3b8ceb4e07705b2d30501f
ildar shafigullin <ildar66@inbox.ru>
2016-11-10 16:06:28 +0300

    add tlog-core module

---
+++ tlog-project/tlog-web/src/main/java/com/tapestry5book/tlog/core/annotations/PublicPage.java
@@ -0,0 +1,9 @@
+package com.tapestry5book.tlog.core.annotations;
+
+import java.lang.annotation.*;
+
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface PublicPage {
+}
