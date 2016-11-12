b642759c8df507a6b590411d6db9469312242a8c
ildar shafigullin <ildar66@inbox.ru>
2016-11-12 23:45:35 +0300

    tlog-blogroll plagin module add

---
+++ tlog-project/plugins/tlog-blogroll/src/test/java/org/example/testapp/pages/SidebarBlocksDemo.java
@@ -0,0 +1,16 @@
+package org.example.testapp.pages;
+
+import com.tapestry5book.tlog.core.services.SidebarBlockSource;
+import org.apache.tapestry5.Block;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.util.List;
+
+public class SidebarBlocksDemo {
+    @Inject
+    private SidebarBlockSource sidebarBlockSource;
+
+    public List<Block> getBlocks() {
+        return sidebarBlockSource.getBlocks();
+    }
+}
---
+++ tlog-project/plugins/tlog-blogroll/src/test/java/org/example/testapp/services/AppModule.java
@@ -0,0 +1,9 @@
+package org.example.testapp.services;
+
+import com.tapestry5book.tlog.core.services.CoreModule;
+import org.apache.tapestry5.ioc.annotations.SubModule;
+
+@SubModule(CoreModule.class)
+public class AppModule {
+
+}
